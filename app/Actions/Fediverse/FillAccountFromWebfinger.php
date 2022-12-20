219953ef7e1c757be76df29a8dd53e06b77dcab3
Matt Isenhower <matt@isenhower.com>
2022-12-19 22:35:45 -0800

    Add FillAccountFromWebfinger action

---
+++ app/Actions/Fediverse/FillAccountFromWebfinger.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Actions\Fediverse;
+
+use App\Models\Fediverse\Account;
+use Illuminate\Support\Facades\Http;
+
+class FillAccountFromWebfinger
+{
+    public function fill(Account $account): Account
+    {
+        [$profileUrl, $dataUrl] = $this->getResource($account);
+
+        $account->profile_url = $profileUrl;
+
+        $data = $this->getData($dataUrl);
+
+        $account->preferred_username = data_get($data, 'preferredUsername');
+        $account->avatar_url = data_get($data, 'icon.url');
+        $account->header_url = data_get($data, 'image.url');
+        $account->display_name = data_get($data, 'name');
+        $account->summary = data_get($data, 'summary');
+
+        return $account;
+    }
+
+    private function getResource(Account $account): array
+    {
+        $url = "https://$account->domain/.well-known/webfinger?resource=acct:$account->handle";
+
+        $response = Http::acceptJson()
+            ->get($url)
+            ->throw();
+
+        $links = collect($response->json('links'));
+
+        return [
+            $links->where('rel', 'http://webfinger.net/rel/profile-page')->value('href'),
+            $links->where('rel', 'self')->value('href'),
+        ];
+    }
+
+    private function getData(string $url): array
+    {
+        $response = Http::acceptJson()
+            ->get($url)
+            ->throw();
+
+        return $response->json();
+    }
+}
---
+++ tests/Feature/Actions/Fediverse/FillAccountViaWebfingerTest.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace Tests\Feature\Actions\Fediverse;
+
+use App\Actions\Fediverse\FillAccountFromWebfinger;
+use App\Models\Fediverse\Account;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Http\Client\Request;
+use Illuminate\Support\Facades\Http;
+use Tests\TestCase;
+
+class FillAccountViaWebfingerTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /** @test */
+    public function it_fills_account_details_from_user_data()
+    {
+        Http::fake([
+            'example.com/.well-known/webfinger?resource=acct:jdoe@example.com' => [
+                'links' => [
+                    [
+                        'rel' => 'http://webfinger.net/rel/profile-page',
+                        'href' => 'https://example.com/users/jdoe/profile',
+                    ],
+                    [
+                        'rel' => 'self',
+                        'href' => 'https://example.com/users/jdoe/self',
+                    ],
+                ],
+            ],
+            'example.com/users/jdoe/self' => [
+                'preferredUsername' => 'JDoe',
+                'icon' => ['url' => 'https://example.net/icon.jpg'],
+                'image' => ['url' => 'https://example.net/image.jpg'],
+                'name' => 'Jane Doe',
+                'summary' => 'Some user bio',
+            ],
+        ]);
+        Http::preventStrayRequests();
+
+        $account = new Account([
+            'username' => 'jdoe',
+            'domain' => 'example.com',
+        ]);
+
+        (new FillAccountFromWebfinger())->fill($account);
+
+        // Make sure all fields are valid
+        $account->save();
+        $account->refresh();
+
+        // Make sure the first request was sent
+        Http::assertSent(function (Request $request) {
+            return $request->url() === 'https://example.com/.well-known/webfinger?resource=acct:jdoe@example.com'
+                && $request->header('Accept') === ['application/json'];
+        });
+        $this->assertSame('https://example.com/users/jdoe/profile', $account->profile_url);
+
+        // Make sure the second request was sent
+        Http::assertSent(function (Request $request) {
+            return $request->url() === 'https://example.com/users/jdoe/self'
+                && $request->header('Accept') === ['application/json'];
+        });
+        $this->assertSame('JDoe', $account->preferred_username);
+        $this->assertSame('https://example.net/icon.jpg', $account->avatar_url);
+        $this->assertSame('https://example.net/image.jpg', $account->header_url);
+        $this->assertSame('Jane Doe', $account->display_name);
+        $this->assertSame('Some user bio', $account->summary);
+    }
+}
