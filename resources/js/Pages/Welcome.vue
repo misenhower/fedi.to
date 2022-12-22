50a9e16689ea22c03c64f9506fcf61174c260e49
Matt Isenhower <matt@isenhower.com>
2022-12-21 18:08:08 -0800

    Update welcome page

--- resources/js/Pages/Welcome.vue
+++ resources/js/Pages/Welcome.vue
@@ -1,3 +1,14 @@
 <template>
-    <div>Fedi.to</div>
+    <div class="min-h-screen flex items-center">
+        <div class="mx-auto max-w-7xl py-16 px-4 sm:py-24 sm:px-6 lg:px-8">
+            <div class="text-center">
+                <p class="mt-1 text-4xl font-bold tracking-tight text-gray-100 sm:text-5xl lg:text-6xl">
+                    Fedi.to
+                </p>
+                <p class="mx-auto mt-5 max-w-xl text-xl text-gray-400">
+                    Coming soon
+                </p>
+            </div>
+        </div>
+    </div>
 </template>
